fe932e4eb2d9101163cb02b53305cb2fbf1f1882
bzhang <bzhang@optaros.com>
2012-02-12 22:55:38 -0500

    fix mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
       end
     end
     
-    def after_inactive_sign_up_path_for(resource)
+    def after_sign_up_path_for(resource)
       flash[:notice]="感谢您的加入！请查收您的邮件确认。"
       new_user_session_path
     end
