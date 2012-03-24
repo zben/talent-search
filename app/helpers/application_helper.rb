33cae34cb58725b31fdd34c3da29efad56622562
bzhang <bzhang@optaros.com>
2012-03-24 16:44:11 -0400

    fixed view bugs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -67,7 +67,7 @@ module ApplicationHelper
   
   def highlight_link text 
     regex = Regexp.new '(https?:\/\/|www\.)(([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?([^\.,\?\!\;\:\-，。？！\s]))'
-    text.gsub!( regex, '<a target="blank" href="http://\2">超链接</a>' )
+    text.gsub!( regex, '<a target="blank" href="http://\2">相关链接</a>' )
     text
   end
 
