92b52acfd2f3d7e5866b5ea711d0416f056754cd
Ben Zhang <bzhang@thoughtbot.com>
2012-04-22 22:32:17 -0400

    enable full log on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,7 @@ Talent::Application.configure do
 
   # See everything in the log (default is :info)
   config.log_level = :debug
+  config.logger = Logger.new(STDOUT)
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
