f427fb500364282cafa0b0079efb291ebc22f5de
Boshu Liu <liuboshu@gmail.com>
2012-05-19 22:19:24 -0400

    fix shout button and email address using by find password

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,7 +7,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "legendben@gmail.com"
+  config.mailer_sender = "i5kongtalent@gmail.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
