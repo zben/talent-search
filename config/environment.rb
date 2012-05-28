ade080076b70a09a655ee2a3073ad36ad22bc892
Ben Zhang <bzhang@thoughtbot.com>
2012-05-28 17:18:58 -0400

    enable project circle

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
+SHOW_PROJECT_CIRCLE = true
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 Encoding.default_internal = Encoding.find("UTF-8")
 
 # Initialize the rails application
 Talent::Application.initialize!
-
 
