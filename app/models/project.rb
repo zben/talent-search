2edf04268b1fa0145a54d913914f0129ef36b18e
Ben Zhang <bzhang@thoughtbot.com>
2012-05-28 17:50:30 -0400

    default path for project logo

--- app/models/project.rb
+++ app/models/project.rb
@@ -31,6 +31,7 @@ class Project
     if Rails.env.production?  
       has_mongoid_attached_file :logo,
         :path => ':project_logo/:id/:style.:extension',
+        :default_url => ':project_logo/:style/missing.png'
         :storage => :s3,
         :bucket => 'talent-search',
         :s3_credentials => {:access_key_id => ENV['S3_KEY'],:secret_access_key => ENV['S3_SECRET']},
